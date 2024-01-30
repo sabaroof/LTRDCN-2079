ef743934b4e3889533dd060a6cad5079160082d5
fwardzic <fwardzic@cisco.com>
2024-01-30 19:39:43 +0000

    URL port fix

--- Demo-App/frontend/frontend_app.py
+++ Demo-App/frontend/frontend_app.py
@@ -1,10 +1,13 @@
 from flask import Flask, jsonify, render_template_string
 import requests
 import os
+import logging
 
 app = Flask(__name__)
 
-BACKEND_URL = 'http://' + os.getenv('BACKEND_URL') + ':' + os.getenv('BACKEND_PORT')
+logging.basicConfig(level=logging.INFO)
+
+BACKEND_URL = 'http://' + os.getenv('BACKEND_URL') + ':' + os.getenv('BACKEND_PORT') + '/version'
 
 @app.route('/')
 def home():
@@ -12,12 +15,16 @@ def home():
         response = requests.get(BACKEND_URL, timeout=5)
         response.raise_for_status()
     except requests.exceptions.HTTPError as errh:
+        app.logger.error(f"HTTP Error occurred: {errh}")
         return f"HTTP Error occurred: {errh}"
     except requests.exceptions.ConnectionError as errc:
+        app.logger.error(f"Error Connecting: {errc}")
         return f"Error Connecting: {errc}"
     except requests.exceptions.Timeout as errt:
+        app.logger.error(f"Timeout Error: {errt}")
         return f"Timeout Error: {errt}"
     except requests.exceptions.RequestException as err:
+        app.logger.error(f"Something went wrong: {err}")
         return f"Something went wrong: {err}"
     else:
         data = response.json()
