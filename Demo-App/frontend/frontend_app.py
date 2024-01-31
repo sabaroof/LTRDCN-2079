cc1f5f3a2097ad24e601f5b2b10d9d71d631c04e
fwardzic <fwardzic@cisco.com>
2024-01-31 09:28:41 +0000

    updates in app url and images

--- Demo-App/frontend/frontend_app.py
+++ Demo-App/frontend/frontend_app.py
@@ -1,18 +1,19 @@
-from flask import Flask, jsonify, render_template_string
+from flask import Flask, jsonify, render_template_string, request, Response
 import requests
 import os
 import logging
+from werkzeug.wrappers import Response as ResponseBase
 
 app = Flask(__name__)
 
 logging.basicConfig(level=logging.INFO)
 
-BACKEND_URL = 'http://' + os.getenv('BACKEND_URL') + ':' + os.getenv('BACKEND_PORT') + '/version' + '/version'
+BACKEND_BASE_URL = 'http://' + os.getenv('BACKEND_URL') + ':' + os.getenv('BACKEND_PORT')
 
 @app.route('/')
 def home():
     try:
-        response = requests.get(BACKEND_URL, timeout=5)
+        response = requests.get(BACKEND_BASE_URL + '/version', timeout=5)
         response.raise_for_status()
     except (requests.exceptions.HTTPError,
             requests.exceptions.ConnectionError,
@@ -22,9 +23,16 @@ def home():
         return f"Error occurred: {err}"
     else:
         data = response.json()
-        image_url = BACKEND_BASE_URL + data['image_url']
-        content = '<body style="background: url(' + image_url + ');">' + data['content'] + '</body>'
+        image_url = '/backend' + data['image_url']
+        content = '<body style="background: url(' + image_url + '); background-size: cover; background-position: center; height: 100vh;">' + data['content'] + '</body>'
+
+
         return render_template_string(content)
+
+@app.route('/backend/static/<path:path>')
+def proxy(path):
+    response = requests.get(BACKEND_BASE_URL + '/static/' + path)
+    return ResponseBase(response.content, mimetype=response.headers['Content-Type'])
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
